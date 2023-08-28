33a59e9fb419754cd7662389746b1cecd57d2cc0
Guilherme Pereira <cc23519@g.unicamp.br>
2023-08-28 13:46:25 -0300

    main

--- JSs/comportamento.js
+++ JSs/comportamento.js
@@ -1,57 +1,66 @@
 function ExcluirContato(li) {
-    // Remove o contato (o pai do botão "Excluir")
     li.remove();
 }
 
 function CriarContato() {
     var Nome = document.getElementById("txtnome").value;
     var Telefone = document.getElementById("txttel").value;
+    var contato = document.createElement("li");
 
-    // Verifique se ambos os campos estão preenchidos
-    if (Nome.trim() === "" || Telefone.trim() === "") {
-        alert("Por favor, preencha todos os campos.");
-        return; // Não faz nada se algum dos campos estiver vazio
-    }
-
-    // Crie um elemento de lista <li> para o contato
-    const contato = document.createElement("li");
-    const editarBotao = document.createElement("button");
-    const excluirBotao = document.createElement("button");
-
-    const nomeElement = document.createElement("span");
+    var nomeElement = document.createElement("span");
     nomeElement.className = "nome";
     nomeElement.textContent = `Nome: ${Nome}`;
 
-    const telefoneElement = document.createElement("span");
-    telefoneElement.className = "telefone";
-    telefoneElement.textContent = `Telefone: ${Telefone}`;
+    var numeroTelefone = document.createElement("span");
+    numeroTelefone.className = "telefone";
+    numeroTelefone.textContent = `| Telefone: ${Telefone}`;
 
-    editarBotao.textContent = "Editar";
-    excluirBotao.textContent = "Excluir";
+    contato.nomeElement = nomeElement;
+    contato.numeroTelefone = numeroTelefone;
 
-    // Adicione o botão de edição ao contato
+    var editarBotao = document.createElement("button");
+    var excluirBotao = document.createElement("button");
+
+    var editarImagem = document.createElement("img");
+    editarImagem.src = "./Imagens/editar.png";
+    editarImagem.style.width = "40px";
+    editarImagem.style.height = "40px";
+
+    var excluirImagem = document.createElement("img");
+    excluirImagem.src = "./Imagens/lixeira.png";
+    excluirImagem.style.width = "40px";
+    excluirImagem.style.height = "40px";
+
+    editarBotao.appendChild(editarImagem);
+    excluirBotao.appendChild(excluirImagem);
+
     editarBotao.addEventListener("click", function () {
         EditarContato(contato);
     });
 
-    // Adicione o botão de exclusão ao contato
     excluirBotao.addEventListener("click", function () {
         ExcluirContato(contato);
     });
 
-    // Adicione elementos ao contato
     contato.appendChild(nomeElement);
-    contato.appendChild(telefoneElement);
+    contato.appendChild(numeroTelefone);
     contato.appendChild(editarBotao);
     contato.appendChild(excluirBotao);
 
-    // Encontre a lista de contatos (você precisa ter uma lista na sua página)
-    const listaContatos = document.getElementById("lista-contatos");
+    var listaContatos = document.getElementById("lista-contatos");
 
-    // Adicione o elemento de contato à lista de contatos
     listaContatos.appendChild(contato);
 
-    // Limpe os campos após adicionar o contato
     document.getElementById("txtnome").value = "";
     document.getElementById("txttel").value = "";
+}
+
+function EditarContato(contato) {
+    var novoNome = prompt("Novo nome:", contato.nomeElement.textContent.replace("Nome: ", ""));
+    var novoTelefone = prompt("Novo telefone:", contato.numeroTelefone.textContent.replace("| Telefone: ", ""));
+
+    if (novoNome !== null && novoTelefone !== null) {
+        contato.nomeElement.textContent = `Nome: ${novoNome}`;
+        contato.numeroTelefone.textContent = `| Telefone: ${novoTelefone}`;
+    }
 }
